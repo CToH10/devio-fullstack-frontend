b0f4c6208ed70be5ecd6caab29508ab0b0c4f8bb
CToH10 <luis.ebn98@gmail.com>
2024-03-21 16:29:02 -0300

    feat: add api connection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        api_base_url: "http://localhost:3001/",
+    },
+};
 
 export default nextConfig;
