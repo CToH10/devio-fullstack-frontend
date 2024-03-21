933a1044caa15765f28ec94c33dfa4e152fa41d2
CToH10 <luis.ebn98@gmail.com>
2024-03-21 10:50:51 -0300

    feat: add basic style structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
@@ -7,14 +8,38 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      green: "#125C13",
+      red: {
+        1: "#D52727",
+        2: "#FAE5E5"
       },
+      grey: {
+        1: "#121212",
+        2:"#5D5D5D",
+        3:"#545454",
+        4: "#6F6F6F",
+        5: "#BCBCBC",
+        6: "#F4F4F4"
+      },
+      whiteFixed: "#FFFFFF",
+    },
+    fontSize: {
+      size_1_44: "2.75rem",
+      size_2_36: "2.25rem",
+      size_3_32: "2rem",
+      size_4_28: "1.75rem",
+      size_5_24: "1.5rem",
+      size_6_20: "1.25rem",
+      size_7_16: "1rem",
+      size_8_14: "0.875rem",
+      btnBig_16: "1rem", 
+      btnMedium_14: "0.875rem",
+      inputPlace_16: "1rem", 
+      inputLabel_14: "0.875rem",
     },
   },
+  
   plugins: [],
 };
 export default config;
