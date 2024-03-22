4529ecea3f48fc46ed4c76e2ebd382ffbb26d143
CToH10 <luis.ebn98@gmail.com>
2024-03-22 17:08:15 -0300

    feat: add cart buttons

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -5,5 +5,25 @@ import { useApi } from '@/context/apiContext';
 export function Cart() {
   const { cart } = useApi();
 
-  return <p>{cart.length}</p>;
+  return (
+    <>
+      <p>{cart.length}</p>
+      <section className="flex flex-row w-full gap-8 justify-end">
+        <button
+          type="button"
+          className="btn-big btn-green-outline"
+          disabled={cart.length === 0}
+        >
+          Cancelar
+        </button>
+        <button
+          type="button"
+          className="btn-big btn-green"
+          disabled={cart.length === 0}
+        >
+          Finalizar pedido
+        </button>
+      </section>
+    </>
+  );
 }
