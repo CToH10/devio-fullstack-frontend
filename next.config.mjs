3178f94bd1fbc92e331db7e8f475ff5199b34378
CToH10 <luis.ebn98@gmail.com>
2024-03-21 23:44:52 -0300

    fix: lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        api_base_url: "http://localhost:3001/",
-    },
+  env: {
+    api_base_url: 'http://localhost:3001/',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
