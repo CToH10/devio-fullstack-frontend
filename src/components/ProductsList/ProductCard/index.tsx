a76c20908c414adf504d9dba3c78109d6b4434a4
CToH10 <luis.ebn98@gmail.com>
2024-03-22 10:05:21 -0300

    feat: product button

--- src/components/ProductsList/ProductCard/index.tsx
+++ src/components/ProductsList/ProductCard/index.tsx
@@ -8,39 +8,43 @@ export function ProductCard({
   name,
   price,
 }: ProductInterface) {
-  // ver como eu fiz o botão na categoria sem perder o brinquedo
-  // mais alto menos gordo nas telas maiores
   return (
     <li
       key={id}
       className="h-64 shadow flex flex-col items-center rounded-xl product-card w-52 max-w-52 md:m-auto lg:m-0"
     >
-      <section className="w-full max-h-24 h-24 rounded-t-xl">
-        <figure className="w-full h-full relative object-contain flex text-center rounded-t-xl">
-          <Image
-            src={image}
-            alt={description}
-            aria-label={description}
-            width={110}
-            height={110}
-            className="object-cover m-auto self-center mt-7 relative z-50"
-          />
-          <figcaption className="hidden">{description}</figcaption>
-        </figure>
-      </section>
-      <section className="bg-whiteFixed rounded-t-lg w-full flex flex-col items-center justify-center h-40 px-3 relative">
-        <section className="flex flex-col justify-between items-center w-full">
-          <p className="font-semibold text-grey-1 text-size_6_20 max-w-full text-nowrap overflow-hidden text-ellipsis">
-            {name}
-          </p>
-          <p className="font-normal text-grey-2 text-size_8_14 max-w-full text-nowrap overflow-hidden text-ellipsis">
-            {description.split(', ')[0]}
+      <button
+        type="button"
+        className="h-64 flex flex-col items-center rounded-xl w-52 max-w-52 md:m-auto lg:m-0"
+        onClick={() => console.log(id)}
+      >
+        <section className="w-full max-h-24 h-24 rounded-t-xl">
+          <figure className="w-full h-full relative object-contain flex text-center rounded-t-xl">
+            <Image
+              src={image}
+              alt={description}
+              aria-label={description}
+              width={110}
+              height={110}
+              className="object-cover m-auto self-center mt-7 relative z-50"
+            />
+            <figcaption className="hidden">{description}</figcaption>
+          </figure>
+        </section>
+        <section className="bg-whiteFixed rounded-t-lg w-full flex flex-col items-center justify-center h-40 px-3 relative">
+          <section className="flex flex-col justify-between items-center w-full">
+            <p className="font-semibold text-grey-1 text-size_6_20 max-w-full text-nowrap overflow-hidden text-ellipsis">
+              {name}
+            </p>
+            <p className="font-normal text-grey-2 text-size_8_14 max-w-full text-nowrap overflow-hidden text-ellipsis">
+              {description.split(', ')[0]}
+            </p>
+          </section>
+          <p className="font-semibold text-grey-1 text-size_6_20 absolute bottom-2">
+            {price}
           </p>
         </section>
-        <p className="font-semibold text-grey-1 text-size_6_20 absolute bottom-2">
-          {price}
-        </p>
-      </section>
+      </button>
     </li>
   );
 }
