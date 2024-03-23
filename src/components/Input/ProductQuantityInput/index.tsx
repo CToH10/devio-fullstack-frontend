a4217855b04b2d9b28f49a1c83e4e5a55639bfdc
CToH10 <luis.ebn98@gmail.com>
2024-03-23 16:33:51 -0300

    feat: add provisory quantity input

--- src/components/Input/ProductQuantityInput/index.tsx
+++ src/components/Input/ProductQuantityInput/index.tsx
@@ -4,11 +4,10 @@ export function ProductQuantityInput() {
       <button
         type="button"
         id="decrement-button"
-        data-input-counter-decrement="quantity-input"
-        className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+        className="bg-green-2 border border-green-2 rounded-[50%] absolute z-20 p-3 h-9 focus:ring-green-1 focus:ring-2 focus:outline-none"
       >
         <svg
-          className="w-3 h-3 text-gray-900 dark:text-white"
+          className="w-4 h-3 text-whiteFixed"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -24,20 +23,18 @@ export function ProductQuantityInput() {
         </svg>
       </button>
       <input
-        type="text"
+        type="number"
         id="quantity-input"
-        data-input-counter
-        className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block w-full py-2.5 h-9 relative z-0 rounded-3xl p-l-4"
         defaultValue="1"
       />
       <button
         type="button"
         id="increment-button"
-        data-input-counter-increment="quantity-input"
-        className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+        className="bg-green-2 border border-green-2 rounded-[50%] right-0 absolute z-20 p-3 h-9 focus:ring-green-1 focus:ring-2 focus:outline-none"
       >
         <svg
-          className="w-3 h-3 text-gray-900 dark:text-white"
+          className="w-4 h-3 text-whiteFixed"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
