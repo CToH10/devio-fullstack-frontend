b00d92b18235d408fedb3d20ba1c729f2ccdebae
CToH10 <luis.ebn98@gmail.com>
2024-03-25 21:29:45 -0300

    fix: lint

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { PaymentStructure } from '@/components/PaymentStructure';
 import { redirect } from 'next/navigation';
+import { PaymentStructure } from '@/components/PaymentStructure';
 // import { queryClient } from '@/components/print/queryClient';
 import { useApi } from '@/context/apiContext';
 // import { QueryClientProvider } from '@tanstack/react-query';
