77afcce6f75cd625a66255653b600860d47f6a63
CToH10 <luis.ebn98@gmail.com>
2024-03-25 21:23:36 -0300

    fix: add disclaimer of comments

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { redirect } from 'next/navigation';
 import { PaymentStructure } from '@/components/PaymentStructure';
+import { redirect } from 'next/navigation';
 // import { queryClient } from '@/components/print/queryClient';
 import { useApi } from '@/context/apiContext';
 // import { QueryClientProvider } from '@tanstack/react-query';
+
+// comments are library recommended way of connecting to printer
 
 export default function Payment() {
   const { payingOrder } = useApi();
